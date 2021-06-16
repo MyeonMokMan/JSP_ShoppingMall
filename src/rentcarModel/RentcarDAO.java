14ec4886f4766f89c3526331086097e9f50e46f7
MyeonMokMan <oniic@naver.com>
2021-06-16 15:44:31 +0900

    JAVA파일
    예악하기 화면에 3개의 자동차를 출력하기 위한 메소드 추가

--- src/rentcarModel/RentcarDAO.java
+++ src/rentcarModel/RentcarDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Vector;
 
 public class RentcarDAO {
 	
@@ -30,5 +31,53 @@ public class RentcarDAO {
 			e.printStackTrace();
 			
 		}
+	}
+	
+	//최신순 3대의 자동차를 리턴하는 메소드
+	public Vector<CarListBean> getSelectCar() {
+		
+		Vector<CarListBean> vec = new Vector<CarListBean>();
+		
+		try {
+			
+			getCon();
+			
+			String sql = "select * from rentcar order by no desc";
+			
+			pstmt = con.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			int count = 0;
+			
+			while(rs.next()) {
+				
+				CarListBean bean = new CarListBean();
+				
+				bean.setNo(rs.getInt(1));
+				bean.setName(rs.getString(2));
+				bean.setCategory(rs.getInt(3));
+				bean.setPrice(rs.getInt(4));
+				bean.setUsepeople(rs.getInt(5));
+				bean.setCompany(rs.getString(6));
+				bean.setImg(rs.getString(7));
+				bean.setInfo(rs.getString(8));
+				
+				vec.add(bean);
+				
+				count++;
+				
+				if(count > 2) {break;}
+			}
+			
+			con.close();
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		return vec;
 	}
 }
