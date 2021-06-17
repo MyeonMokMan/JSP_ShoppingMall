b734fae9aa8bddab697a77ae494c515b84489d55
MyeonMokMan <oniic@naver.com>
2021-06-17 15:34:50 +0900

    JAVA파일
    사용자가 예약을 할 때 필요한 Bean.

---
+++ src/rentcarModel/CarReserveBean.java
@@ -0,0 +1,65 @@
+package rentcarModel;
+
+public class CarReserveBean {
+	
+	private int no;
+	private int qty;
+	//대여 기간
+	private int dDay;
+	//대여일 (Date타입 x)
+	private String rDay;
+	private int useIn;
+	private int useWifi;
+	private int useNavi;
+	private int useSeat;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public int getdDay() {
+		return dDay;
+	}
+	public void setdDay(int dDay) {
+		this.dDay = dDay;
+	}
+	public String getrDay() {
+		return rDay;
+	}
+	public void setrDay(String rDay) {
+		this.rDay = rDay;
+	}
+	public int getUseIn() {
+		return useIn;
+	}
+	public void setUseIn(int useIn) {
+		this.useIn = useIn;
+	}
+	public int getUseWifi() {
+		return useWifi;
+	}
+	public void setUseWifi(int useWifi) {
+		this.useWifi = useWifi;
+	}
+	public int getUseNavi() {
+		return useNavi;
+	}
+	public void setUseNavi(int useNavi) {
+		this.useNavi = useNavi;
+	}
+	public int getUseSeat() {
+		return useSeat;
+	}
+	public void setUseSeat(int useSeat) {
+		this.useSeat = useSeat;
+	}
+	
+}
