b2707cf2b5ef98dccd61ae132232351edd098053
MyeonMokMan <oniic@naver.com>
2021-06-18 16:54:28 +0900

    JAVA파일
    예약 확인 빈클래스

---
+++ src/rentcarModel/CarViewBean.java
@@ -0,0 +1,77 @@
+package rentcarModel;
+
+public class CarViewBean {
+	
+	private String name;
+	private int price;
+	private String img;
+	private int qty;
+	private int dDay;
+	private String rDay;
+	private int useIn;
+	private int useWifi;
+	private int useNavi;
+	private int useSeat;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public int getdDay() {
+		return dDay;
+	}
+	public void setdDay(int dDay) {
+		this.dDay = dDay;
+	}
+	public String getrDay() {
+		return rDay;
+	}
+	public void setrDay(String rDay) {
+		this.rDay = rDay;
+	}
+	public int getUseIn() {
+		return useIn;
+	}
+	public void setUseIn(int useIn) {
+		this.useIn = useIn;
+	}
+	public int getUseWifi() {
+		return useWifi;
+	}
+	public void setUseWifi(int useWifi) {
+		this.useWifi = useWifi;
+	}
+	public int getUseNavi() {
+		return useNavi;
+	}
+	public void setUseNavi(int useNavi) {
+		this.useNavi = useNavi;
+	}
+	public int getUseSeat() {
+		return useSeat;
+	}
+	public void setUseSeat(int useSeat) {
+		this.useSeat = useSeat;
+	}
+	
+}
